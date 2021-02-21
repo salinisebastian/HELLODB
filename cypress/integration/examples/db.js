413a76bde617d0c931593380226a6df5a403713f
salinisebastian <salini.seb@gmail.com>
2021-02-21 23:34:59 +0100

    Database testing with postgres

---
+++ cypress/integration/examples/db.js
@@ -0,0 +1,24 @@
+describe("My First Test Suite", function () {
+  it("My FirstTest case", function () {
+    cy.task("dbQuery", { query: "SELECT * FROM persons" }).then(function (
+      result
+    ) {
+      console.log("asdadsas");
+      console.log(result[0].firstname);
+    });
+  });
+
+  it("Secondtest", () => {
+    cy.task("dbQuery", {
+      query: " UPDATE persons SET city = 'Kochi' WHERE personid = 2",
+    });
+
+    // your cy assertions here
+  });
+  it("Thirdtest", () => {
+    cy.task("dbQuery", {
+      query:
+        "insert into persons values ('4','amal','hiiterstraze 49','Stockholm')",
+    });
+  });
+});
